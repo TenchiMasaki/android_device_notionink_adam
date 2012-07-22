1835b09235c7e5d18ced73747ac2d2c5027917bf
Boris Popov <bvpopov@gmail.com>
2012-07-22 02:52:25 -0700

    Fix for lights lib

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -20,12 +20,14 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lights.c
 
 
+LOCAL_PRELINK_MODULE := false
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_MODULE := lights.harmony
-
-LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
