b09dc3abc1f64a71516f21a6b5fe70c5a936b335
Boris Popov <bvpopov@gmail.com>
2012-08-02 23:26:09 -0700

    Misc update lunch now is cm_adam

--- cm.mk
+++ cm.mk
@@ -1,28 +1,43 @@
-PRODUCT_RELEASE_NAME := ADAM
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Camera
+PRODUCT_PACKAGES := \
+    Camera \
+    SpareParts \
+    PQiToggle \
+    Development \
+    Stk \
+    Mms
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from ADAM device
+$(call inherit-product, device/notionink/adam/device.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/notionink/adam/device-vendor.mk)
+
+PRODUCT_NAME := cm_adam
+PRODUCT_DEVICE := adam
+PRODUCT_BRAND := NotionInk
+PRODUCT_MODEL := Notion Ink ADAM
+
+$(call inherit-product, device/notionink/adam/google_apps.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit device configuration.
-$(call inherit-product, device/notionink/adam/full_adam.mk)
-
-# Themes
-PRODUCT_PACKAGES = \
-        ThemeManager \
-        ThemeChooser \
-        com.tmobile.themes \
-        Androidian \
-        Cyanbread
-
-# CM
-PRODUCT_PACKAGES = \
-	Superuser \
- 	Trebuchet \
-	su \
-	CMParts
-
-# Change these to values from a stock ADAM rom
-#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_ID=HTK75 BUILD_DISPLAY_ID=HTK75 BUILD_FINGERPRINT="asus/WW_epad/EeePad:3.2.1/HTK75/WW_epad-8.6.5.18-20111028:user/release-keys" PRVIATE_BUILD_DESC="WW_epad-user 3.2.1 HTK75 WW_epad-8.6.5.18-20111028 release-keys"
-
-PRODUCT_NAME := cm_adam
-PRODUCT_DEVICE := adam
